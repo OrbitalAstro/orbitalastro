03491d6c3ff978450ad023ec5d4ffff51038726f
Isabelle Fortier <orbitalastro10@gmail.com>
2026-01-31 09:30:15 -0500

    Harmoniser la taille de police italique des noms à 18pt pour Lecture 2026 (comme Dialogue)

--- web/app/reading-2026/Reading2026Pdf.tsx
+++ web/app/reading-2026/Reading2026Pdf.tsx
@@ -158,7 +158,7 @@ const styles = StyleSheet.create({
   inlineScript: {
     fontFamily: greatVibesLoaded ? 'GreatVibes' : 'Times-Italic',
     fontStyle: greatVibesLoaded ? 'normal' : 'italic',
-    fontSize: 12,
+    fontSize: 18,
   },
 })
 
